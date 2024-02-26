76976b46f94cb36747114dd3cc69c915f00163a3
Travis van der F <travis.font@gmail.com>
2024-02-26 12:27:31 +0100

    Create manager.php

---
+++ packages/libraries/manager.php
@@ -0,0 +1,20 @@
+<?php
+
+// this file manages (provider) core required Claritie libraries (projects)
+
+## Components
+
+# eephplib
+# stashqueries
+# ajbdev/cronlingo (Cron-Manager)
+# yosymfony/toml (TOML-Parser)
+
+## Libraries
+
+# PHPMailer/PHPMailer
+# JBZoo/SimpleTypes (ValueObjects)
+# smartemailing/types
+# izniburak/pdox (DB)
+# byjg/php-migration (DB)
+# nette/forms
+# t-regx/T-Regx
