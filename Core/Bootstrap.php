768da14d54074b4c6237ed56a38992100beae75c
Travis van der Font <travis.font@gmail.com>
2025-05-23 23:53:47 +0200

    Framework Core - Initial Bootstrap

--- Core/Bootstrap.php
+++ Core/Bootstrap.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace App\Core;
+
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+class Bootstrap
+{
+    public static $twig;
+    private const VIEW_DIRECTORY = '/../Views';
+
+    public static function init()
+    {
+        require_once __DIR__ . '/Autoloader.php';
+        $loader = new FilesystemLoader(__DIR__ . self::VIEW_DIRECTORY);
+        self::$twig = new Environment($loader);
+    }
+}
