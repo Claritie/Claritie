d6c817ad31103c1476f604b4f45246b9ed972baf
Travis van der Font <travis.font@gmail.com>
2025-05-23 23:54:42 +0200

    Framework Core - Initial Dispatcher

--- Core/Dispatcher.php
+++ Core/Dispatcher.php
@@ -1,1 +1,54 @@
 <?php
+namespace App\Core;
+
+class Dispatcher
+{
+    // @TODO reduce this function into private methods
+    public static function handle($method, $uri)
+    {
+        $parts = explode('/', trim($uri, '/'));
+
+        if (count($parts) < 2)
+        {
+            http_response_code(404);
+            echo "Invalid route.";
+
+            return;
+        }
+
+        $eventPath      = "Events/" . implode('/', array_slice($parts, 0, -1));
+        $eventClassName = ucfirst(end($parts));
+        $fullPath       = __DIR__ . "/../$eventPath/$eventClassName.php";
+
+        if (!file_exists($fullPath))
+        {
+            http_response_code(404);
+            echo "Event not found.";
+            return;
+        }
+
+        require_once $fullPath;
+
+        $fqcn = "App\\$eventPath\\$eventClassName";
+
+        if (!class_exists($fqcn))
+        {
+            http_response_code(500);
+            echo "Class $fqcn not found.";
+
+            return;
+        }
+
+        $instance = new $fqcn();
+
+        if (!method_exists($instance, strtolower($method)))
+        {
+            http_response_code(405);
+            echo "Method not allowed.";
+
+            return;
+        }
+
+        call_user_func_array([$instance, strtolower($method)], array_slice($parts, 2));
+    }
+}
