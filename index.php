2d44b28ba4d5236bb7b10cb2bfbe6aa75801210b
Travis van der Font <travis.font@gmail.com>
2025-05-24 00:28:19 +0200

    initial index

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+
+use App\Core\Bootstrap;
+use App\Core\Dispatcher;
+
+require_once __DIR__ . '/Packages/autoload.php';
+
+Bootstrap::init();
+
+$method = $_SERVER['REQUEST_METHOD'];
+$uri = $_GET['route'] ?? '';
+
+Dispatcher::handle($method, $uri);
