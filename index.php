ad97e01b48046a788427871769f3f005cd6afa48
Travis van der Font <travis.font@gmail.com>
2025-05-25 16:18:34 +0200

    Update - To handle new and cleaner routing URLs

--- index.php
+++ index.php
@@ -5,9 +5,11 @@ use App\Core\Dispatcher;
 
 require_once __DIR__ . '/Packages/autoload.php';
 
+session_start();
 Bootstrap::init();
 
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
-$uri = $_GET['route'] ?? '';
+//$uri = $_GET['route'] ?? '';
 
-Dispatcher::handle($method, $uri);
+Dispatcher::handle($method, $requestUri);
