6365c79603381f354312a021eaced0342d68546f
Travis van der Font <travis.font@gmail.com>
2025-05-25 16:15:08 +0200

    variable updates

--- Core/Bootstrap.php
+++ Core/Bootstrap.php
@@ -7,13 +7,18 @@ use Twig\Environment;
 
 class Bootstrap
 {
-    public static $twig;
+    public static Environment $twig;
     private const VIEW_DIRECTORY = '/../Views';
 
     public static function init()
     {
         require_once __DIR__ . '/Autoloader.php';
-        $loader = new FilesystemLoader(__DIR__ . self::VIEW_DIRECTORY);
-        self::$twig = new Environment($loader);
+
+        $twigLoader = new FilesystemLoader(__DIR__ . self::VIEW_DIRECTORY);
+        $twigOptionSet = [
+            'cache' => '/path/to/compilation_cache',
+        ];
+
+        self::$twig = new Environment($twigLoader);
     }
 }
